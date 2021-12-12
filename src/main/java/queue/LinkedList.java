a3ccbf4b6f457f35eb6c41f5b694d2c7145d49e7
deepakphpweb <dipakrana2760@gmail.com>
2021-12-13 00:26:39 +0530

    Added : Create push and pon in queue

---
+++ src/main/java/queue/LinkedList.java
@@ -0,0 +1,57 @@
+package queue;
+
+public class LinkedList {
+    Node head;
+    Node tail;
+
+    public Node push(int data) {
+        Node node = new Node(data);
+        if (head == null) {
+            this.head = node;
+        } else {
+            tail.next = node;
+        }
+        this.tail = node;
+        return node;
+    }
+
+    public void printList() {
+        Node node = head;
+        if (head == null) {
+            System.out.println("List is empty");
+        } else {
+            while (node != null) {
+                if (node.next != null) {
+                    System.out.print(node.data + " -> ");
+                } else
+                    System.out.println(node.data);
+                node = node.next;
+            }
+        }
+    }
+
+    public Node peek() {
+        Node node = head;
+        if (head == null) {
+            System.out.println("List is empty");
+        } else {
+            while (node.next != null) {
+                node = node.next;
+            }
+        }
+        return node;
+    }
+
+    public Node pop() {
+        Node node = this.head;
+        while (node.next != null) {
+            System.out.println("Popped Element : "+node.next.next.data);
+            node = node.next;
+            node.next = null;
+
+        }
+
+        return node;
+
+    }
+}
---
+++ src/main/java/queue/Stack.java
@@ -0,0 +1,37 @@
+package queue;
+
+public class Stack {
+    public Node top;
+    LinkedList linkedList;
+
+
+    public Stack() {
+        this.linkedList = new LinkedList();
+    }
+
+    public void push() {
+        linkedList.push(70);
+        linkedList.push(30);
+        top = linkedList.push(56);
+        linkedList.printList();
+        System.out.println("Top - > " + top.data);
+    }
+
+    public void peek() {
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+        top=linkedList.peek();
+        System.out.println("Top - > " + top.data);
+    }
+
+    public void pop() {
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+        top=linkedList.pop();
+        System.out.println("Top - > " + top.data);
+        System.out.println("After ------ ");
+        linkedList.printList();
+    }
+}
